befb4b2937df913edcace0d695666c292b608783
inf111 <inf111@mail.ru>
2023-05-23 01:56:37 +0300

    minor

--- text_to_img.py
+++ text_to_img.py
@@ -3,7 +3,11 @@
 
 from diffusers import DiffusionPipeline
 
-pipeline = DiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5")
+#pipeline = DiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5")
+pipeline = DiffusionPipeline.from_pretrained("/home/ubuntu/diffusers/conv/2")
 pipeline.to("cuda")
-image = pipeline("A pokemon with blue eyes.").images[0]
-image.save("pokemon5.png")
+
+imgs = pipeline(prompt = "An image of a lkjh ring in Picasso style", num_images_per_prompt=5).images;
+
+for index, item in enumerate(imgs):
+   item.save(f"Picasso-{index}.png")
